8a16af0e95fdee716d78f3a993976f5ae004e7f1
carmen0810 <66385122+Hiald@users.noreply.github.com>
2021-06-11 18:57:02 -0500

    cambios

---
+++ CambioTotalFrontEnd/CambioTotalED/eddivisa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CambioTotalED
+{
+    public class edDivisa
+    {
+        // i, id, b = int 
+        // v, dt = string
+        // d = decimal
+        public int iddivisa { get; set; }
+        public int idusuario { get; set; }
+        public int itipobusqueda { get; set; }
+        public decimal dmonto { get; set; }
+        public decimal dsolesventa { get; set; }
+        public decimal dsolescompra { get; set; }
+        public decimal ddolaresventa { get; set; }
+        public decimal ddolarescompra { get; set; }
+        public int itipopromocion { get; set; }
+        public string dtfecha { get; set; }
+        public string vhora { get; set; }
+        public string dtfecharegistro { get; set; }
+        public string dtfechamodificacion { get; set; }
+        public string horamodificacion { get; set; }
+
+
+
+    }
+}
---
+++ CambioTotalFrontEnd/CambioTotalTD/tdOperacion.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CambioTotalED;
+using CambioTotalAD;
+using MySql.Data.MySqlClient;
+
+namespace CambioTotalTD
+{
+    public class tdOperacion:td_aglobal
+    {
+        adOperacion iadOperacion;
+
+        public int tdOperacionCuentaBancaria(int tdtipoOperacion, int tdidCuentBanc, int tdidusuario, int tditipocuenta, int tdimoneda
+                                        , int tdibanco, string tdbanco, string tdnumerocuenta, string tdnombrecuenta, int tditipodeclaracion
+                                        , string tdtitular, string tdfecReg, string tdhoraReg)
+        {
+            int iRespuesta = -1;
+            try
+            {
+                using (MySqlConnection con = new MySqlConnection(mysqlConexion))
+                {
+                    con.Open();
+                    using (MySqlTransaction scope = con.BeginTransaction())
+                    {
+                        iadOperacion = new adOperacion(con);
+                        iRespuesta = iadOperacion.adOperacionCuentaBancaria(tdtipoOperacion,tdidCuentBanc, tdidusuario, tditipocuenta, tdimoneda
+                                        , tdibanco, tdbanco, tdnumerocuenta, tdnombrecuenta, tditipodeclaracion
+                                        , tdtitular, tdfecReg, tdhoraReg);
+                        scope.Commit();
+                    }
+                }
+                return (iRespuesta);
+            }
+            catch (MySqlException ex)
+            {
+                //UtlLog.toWrite(UtlConstantes.TProcessRN, UtlConstantes.LogNamespace_TProcessRN, this.GetType().Name.ToString(), MethodBase.GetCurrentMethod().Name, UtlConstantes.LogTipoError, "", ex.StackTrace.ToString(), ex.Message.ToString());
+                throw ex;
+            }
+
+        }
+
+    }
+}
